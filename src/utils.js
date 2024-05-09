192f9df0ff75704c192eeb586ad84a6a3ca40de0
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-09 18:51:24 -0500

    Add error function

---
+++ src/utils.js
@@ -0,0 +1,14 @@
+const ERRORS = [
+  {
+    id: "name",
+    msg: "Please enter your full name.",
+    validate(val) {
+      return val.length > 6 && val.includes(" ");
+    },
+  },
+  { id: "email", msg: "Please enter a valid email address." },
+  {
+    id: "message",
+    msg: "Please enter a message between 10 and 100 characters.",
+  },
+];
