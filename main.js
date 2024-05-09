7e258ad843b81c698700d5cedd2b079159c4add4
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-09 18:06:57 -0500

    Add formEls array to get all form controls

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function openIndex() {
-    window.location.href = "index.html";
+  window.location.href = "index.html";
 }
+
+const formEls = [
+  ...Array.from(document.querySelectorAll("input")),
+  document.querySelector("#message"),
+];
+console.log(formEls);
