08a1431265394c2197c1337c8409b3443235e78c
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-11 20:47:36 -0500

    Change renderError to reference element instead of parent (the whole form class)

--- src/utils.js
+++ src/utils.js
@@ -29,11 +29,10 @@ export const hideError = (el) => {
 };
 
 export const renderError = (el, msg) => {
-  const elParent = el.parentNode;
+  const elParent = el.nextElementSibling;
 
   // Did we already render an error?
-  const errorEl = el.parentNode.querySelector(".error");
-
+  const errorEl = el.nextElementSibling;
   // If so, add the class to show the error message
   if (errorEl) errorEl.classList.add("is-error");
   // Otherwise, add the error message from scratch
