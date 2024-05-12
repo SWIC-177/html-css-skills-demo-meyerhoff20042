1f467934b6541b717339661801b376a0f7e3078b
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-11 21:34:40 -0500

    Add new validation methods

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import validator from "validator";
 import { ERRORS, hideError, renderError } from "./src/utils";
 
 console.log(ERRORS);
@@ -12,8 +13,6 @@ const formEls = [
 ];
 console.log(formEls);
 
-const submitBtn = document.querySelector("button[type='submit']");
-
 formEls.forEach((el) => {
   el.addEventListener("blur", (e) => {
     const elError = ERRORS.find((error) => error.id === e.target.id);
@@ -21,3 +20,43 @@ formEls.forEach((el) => {
     else hideError(e.target);
   });
 });
+
+export function submitForm() {
+  // Get form elements
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const messageInput = document.getElementById("message");
+
+  // Validate each input element
+  if (!validateName(nameInput)) {
+    alert("Please enter your full name.");
+    console.log("name check");
+    return false;
+  }
+
+  if (!validateEmail(emailInput)) {
+    alert("Please enter a valid email address.");
+    console.log("email check");
+    return false;
+  }
+
+  if (!validateMessage(messageInput)) {
+    alert("Please enter a message between 10 and 100 characters.");
+    console.log("message check");
+    return false;
+  }
+
+  return true;
+}
+
+function validateName(input) {
+  return input.length > 6 && input.includes(" ");
+}
+
+function validateEmail(input) {
+  return validator.isEmail(input);
+}
+
+function validateMessage(input) {
+  return input.length >= 10 && input.length <= 100;
+}
